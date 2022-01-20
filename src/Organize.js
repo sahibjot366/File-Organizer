8f01beee972aa44eb7d1c2b7306d7917d2f489fb
Sahibjot Singh <sahibjot366@gmail.com>
2022-01-21 02:55:49 +0530

    Updates in Organize.js

--- src/Organize.js
+++ src/Organize.js
@@ -6,6 +6,25 @@ const types = {
   Videos: ["mp4", "mkv", "mov", "wmv", "flv", "avi", "webm", "avchd"],
   Archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
   Pictures: ["jpg", "jpeg", "png", "tiff", "bmp", "gif", "eps"],
+  "Programming Files": [
+    "py",
+    "js",
+    "css",
+    "c",
+    "cpp",
+    "java",
+    "asp",
+    "aspx",
+    "pl",
+    "php",
+    "xml",
+    "php3",
+    "php4",
+    "svg",
+    "html",
+    "htm",
+    "json",
+  ],
   Documents: [
     "docx",
     "ppt",
@@ -46,7 +65,6 @@ const arrangeFiles = (file, fileSrcPath, organizedFilesDirPath) => {
   const category = getCategory(file);
   assureFolderExistance(organizedFilesDirPath, category); // assures that category folder will be created
   const fileDestPath = path.join(organizedFilesDirPath, category, file);
-  console.log(fileSrcPath, fileDestPath);
   fs.copyFileSync(fileSrcPath, fileDestPath);
   fs.unlinkSync(fileSrcPath);
 };
@@ -56,16 +74,24 @@ const OrganizeFiles = (dirpath) => {
     console.log("Invalid path entered!");
     return;
   }
+  console.log("Starting...");
   assureFolderExistance(dirpath, "Organized Files"); // that organized folder exists, if doesn't exist then it will be created
   const organizedFilesDirPath = path.join(dirpath, "Organized Files");
   const dirItems = fs.readdirSync(dirpath);
 
+  let count = 0;
   dirItems.forEach((item) => {
     const itemPath = path.join(dirpath, item);
     if (fs.lstatSync(itemPath).isFile()) {
+      count++;
       arrangeFiles(item, itemPath, organizedFilesDirPath); // copies file to organized folder and deletes files in unorganized folder
     }
   });
+  if (count == 0) {
+    console.log("Nothing to sort or this directory is already sorted!");
+  } else {
+    console.log("Done...");
+  }
 };
 
 module.exports = {
